2513fbb5726d36328cbc699c7051cf65b0f213dd
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-04-07 22:21:58 +1000

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+struct Node<T> {
+    data: T,
+    next: Option<Box<Node<T>>> ,
+}
+
+impl <T> Node<T> {
+    fn new(data: T, next: Option<Box<Node<T>>>) {
+
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
