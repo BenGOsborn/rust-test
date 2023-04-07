21ef4ad2b6ea95bab81897794e84b0e20f86ccaa
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-04-08 08:07:30 +1000

    Created stack struct impl

--- src/main.rs
+++ src/main.rs
@@ -26,14 +26,42 @@ impl<T> Stack<T> {
     }
 
     fn push(&mut self, data: T) {
-        let temp = Some(Box::new(Node::new(data)));
-        temp.next = self.head;
+        let mut temp = Box::new(Node::new(data));
+        temp.next = self.head.take();
 
-        self.head = temp;
+        self.head = Some(temp);
+        self.length += 1;
+    }
+
+    fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.data)
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|boxed_node| {
+            let node = boxed_node;
+
+            self.head = node.next;
+            self.length -= 1;
+
+            node.data
+        })
     }
 }
 
 fn main() {
     // let mut head = Node::new(3);
     let mut stack: Stack<i32> = Stack::new();
+
+    stack.push(2);
+    stack.push(4);
+
+    println!("{:?}", stack.peek());
+    println!("{:?}", stack.length);
+
+    println!("{:?}", stack.pop());
+    println!("{:?}", stack.pop());
+    println!("{:?}", stack.length);
+
+    println!("{:?}", stack.pop());
 }
