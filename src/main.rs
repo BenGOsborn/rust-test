c5791a6f5147f059627c5e998567a0944c1daafe
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-04-07 22:37:42 +1000

    Created node and creating stack data structure

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
 struct Node<T> {
     data: T,
-    next: Option<Box<Node<T>>> ,
+    next: Option<Box<Node<T>>>,
 }
 
-impl <T> Node<T> {
-    fn new(data: T, next: Option<Box<Node<T>>>) {
+impl<T> Node<T> {
+    fn new(data: T) -> Self {
+        Node {
+            data: data,
+            next: None,
+        }
+    }
+}
 
+struct Stack<T> {
+    length: usize,
+    head: Option<Box<Node<T>>>,
+}
+
+impl<T> Stack<T> {
+    fn new() -> Self {
+        Stack {
+            length: 0,
+            head: None,
+        }
+    }
+
+    fn push(&mut self, data: T) {
+        let temp = Some(Box::new(Node::new(data)));
+        temp.next = self.head;
+
+        self.head = temp;
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    // let mut head = Node::new(3);
+    let mut stack: Stack<i32> = Stack::new();
 }
