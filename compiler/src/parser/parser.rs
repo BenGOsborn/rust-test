db4579645c781ff4695b81a6f22724db32867833
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-04-08 12:24:50 +1000

    Added comments

--- compiler/src/parser/parser.rs
+++ compiler/src/parser/parser.rs
@@ -1,5 +1,8 @@
 use crate::tokenizer::token::Token;
 
 pub fn parse(tokens: &Vec<Token>) -> i32 {
+    // **** This will require looping through the tokens, creating a stack structure, validating the stack as we go, and returning the output
+    // **** In addition, enable the option to debug where the value will be printed
+
     -1
 }
